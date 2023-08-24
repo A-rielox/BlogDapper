0260f50a7265845c1813011d204f011868a6bf8d
A-rielox <arielox.ag@gmail.com>
2023-08-23 18:04:59 -0600

    Add project files.

---
+++ BlogDapper/Repositorio/ICategoriaRepositorio.cs
@@ -0,0 +1,17 @@
+﻿using BlogDapper.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BlogDapper.Repositorio;
+
+public interface ICategoriaRepositorio
+{
+    Categoria GetCategoria(int id);
+    List<Categoria> GetCategorias();
+    Categoria CrearCategoria(Categoria categoria);
+    Categoria ActualizarCategoria(Categoria categoria);
+    void BorrarCategoria(int id);
+
+    //Método especial para el dropdown con la lista de categorías en la vista artículos,
+    //se debe crear aquí para invocarse desde el controlador artículos
+    //IEnumerable<SelectListItem> GetListaCategorias();
+}
